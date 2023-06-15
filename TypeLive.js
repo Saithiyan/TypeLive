822d96c3e5e7bb3d4c4ca7dbea7a804c357d6596
Saithiyan <saiketheeswaran@gmail.com>
2023-06-15 10:18:38 +0200

    add method (removeAllText, startRemoveAllText) and cleaned code

--- TypeLive.js
+++ TypeLive.js
@@ -2,80 +2,50 @@ class TypeLive {
   constructor(selectedElement) {
     this.selectedElement = document.querySelector(selectedElement);
   }
-
   typing(text, speed, i = 0) {
     i;
-    // let text = text;
     if (i < text.length) {
       this.selectedElement.textContent += text.charAt(i);
-
-      //   console.log(this.selectedElement.textContent,text.charAt(i),i)
-
       i++;
-
       setTimeout(() => this.typing(text, speed, i), speed);
     }
   }
+
   removeAllText(speed, i = 0) {
     let length = this.selectedElement.textContent.length;
-    console.log(length);
     i = length;
     if (length > 0) {
       this.selectedElement.textContent =
         this.selectedElement.textContent.substring(0, length - 1);
-      console.log(this.selectedElement.textContent, length);
       length--;
+    } else {
+      return 0;
     }
     setTimeout(() => {
-      this.removeText(speed, i);
+      this.removeAllText(speed, i);
     }, speed);
     return this;
   }
+
   removeText(text, speed) {
     let textLength = typeof text === "string" ? text.length : text;
-
-    // let textLength;
-    // if (text === "All") {
-    //   textLength = this.selectedElement.textContent.length;
-    // } else if (typeof text === "string") {
-    //   textLength = text.length;
-    // } else if ( typeof text === "number") {
-    //   textLength = text;
-    // }
-
-    // let textLength = text.length;
     let length = this.selectedElement.textContent.length;
-    // console.log(text, textLength, length);
 
     if (textLength > 0) {
       this.selectedElement.textContent =
         this.selectedElement.textContent.substring(0, length - 1);
-
       textLength--;
-      // console.log(this.selectedElement.textContent, textLength);
     } else if (textLength == 0) {
-      console.log("tl=0 donc return 0");
       return 0;
     }
-
-
     setTimeout(() => {
       let newText =
         typeof text === "string" ? text.substring(0, textLength) : textLength;
-
-      // let newText
-      // if (text === "All") {
-      //   newText = this.selectedElement.textContent.length;
-      // } else if (typeof text === "string") {
-      //   newText = text.substring(0, textLength);
-      // } else {
-      //   newText = text;
-      // }
-      // let newText = text.substring(0, textLength);
-      // console.log(newText, "dansSetTimeout", textLength);
       this.removeText(newText, speed);
     }, speed);
   }
+
+  //Method with option delai (setTimeout):
 
   startTyping(text, speed, delai = 0) {
     setTimeout(() => {
@@ -83,17 +53,12 @@ class TypeLive {
     }, delai);
     return this;
   }
-
   startRemove(text, speed, dalai) {
     setTimeout(() => {
       this.removeText(text, speed);
     }, delai);
     return this;
   }
-  
-  // loop( delai){
-  //   setInterval(()=>{},delai)
-  // }
   startRemoveAllText(speed, dalai) {
     setTimeout(() => {
       this.removeAllText(speed);
